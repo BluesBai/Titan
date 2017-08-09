2f3a1e5e03fa2b4cdf750dfc9a8ddde2b9fdbbb3
Zeren Bai <bluesbai1992@gmail.com>
2017-08-09 10:15:31 -0400

    Add Login Logout and session function

--- src/rpc/RecommendItem.java
+++ src/rpc/RecommendItem.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.json.JSONArray;
 
 import algorithm.Recommendation;
@@ -35,7 +37,13 @@ public class RecommendItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String userId = request.getParameter("user_id");
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null) {
+			response.setStatus(403);
+			return;
+		}
+		
+		String userId = session.getAttribute("user").toString();
 		double lat = Double.parseDouble(request.getParameter("lat"));
 		double lon = Double.parseDouble(request.getParameter("lon"));
 		Recommendation recommendation = new GeoRecommendation();
